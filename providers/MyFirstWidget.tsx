7f103747ceb52fd395eb4eb8574c210815dd6d67
Colin Kerr <33028649+ColinKerr@users.noreply.github.com>
2023-02-21 22:04:10 -0500

    Add documentation about adding reality data to the view

---
+++ providers/MyFirstWidget.tsx
@@ -0,0 +1,45 @@
+import { useActiveViewport } from "@itwin/appui-react";
+import React, { useEffect } from "react";
+import RealityDataApi from "./RealityDataApi";
+import "./MyFirstWidget.css";
+import { ToggleSwitch } from "@itwin/itwinui-react";
+import { ContextRealityModelProps } from "@itwin/core-common";
+
+export const MyFirstWidget: React.FC = () => {
+  const viewport = useActiveViewport();
+
+  const [initialized, setInitialized] = React.useState<boolean>(false);
+  const [realityModels, setRealityModelList] = React.useState<ContextRealityModelProps[]>([]);
+
+  useEffect(() => {
+    const asyncInitialize = async () => {
+      if (viewport) {
+        const realityModels = await RealityDataApi.getRealityModels(viewport.iModel);
+        setRealityModelList(realityModels);
+      }
+    };
+
+    if (!initialized) {
+      void asyncInitialize().then (() => { setInitialized(true);})
+    }
+  });
+
+  const togglePhillyReality = async (e:React.ChangeEvent<HTMLInputElement>) => {
+    if (viewport) {
+      for (const model of realityModels) {
+        if (model.name === "Philadelphia_2015") {
+          RealityDataApi.toggleRealityModel(model, viewport, e.target.checked);
+        }
+      }
+    }
+  }
+
+  return (
+    <div>
+      This is my first widget
+      <ToggleSwitch onChange={togglePhillyReality} label='Philly Reality Data' />
+    </div>
+  );
+};
+
+
